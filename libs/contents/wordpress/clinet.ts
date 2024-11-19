875d4529e81314d75a2c1b3b3a981b2a0212b110
jaison-codeface <jaison@codefacetech.com>
2024-11-19 16:36:07 +0530

    Update clinet.ts

--- libs/contents/wordpress/clinet.ts
+++ libs/contents/wordpress/clinet.ts
@@ -1,4 +1,4 @@
-const API_URL = "https://lga.ddhotel.in/graphql";
+const API_URL = "https://admin.lgadxb.com/graphql";
 const WORDPRESS_AUTH_REFRESH_TOKEN = "";
 
 export { API_URL, WORDPRESS_AUTH_REFRESH_TOKEN };
