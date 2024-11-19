f14a29ee01b69f46f5909dfa846f3074810f6bbf
jaison-codeface <jaison@codefacetech.com>
2024-11-19 17:38:54 +0530

    Update clinet.ts

--- libs/contents/wordpress/clinet.ts
+++ libs/contents/wordpress/clinet.ts
@@ -1,4 +1,4 @@
-const API_URL = "https://admin.lgadxb.com/graphql";
+const API_URL = "https://cfuat.in/lgaadmin/graphql";
 const WORDPRESS_AUTH_REFRESH_TOKEN = "";
 
 export { API_URL, WORDPRESS_AUTH_REFRESH_TOKEN };
